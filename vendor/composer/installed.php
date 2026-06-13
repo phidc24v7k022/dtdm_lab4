272fff00029a57b3e6ed6e9c998c057bf0767e4e
hoangphi <philh@onland.tech>
2026-06-13 08:28:31 +0700

    add testdb

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'phidc24v7k022/dtdm_lab4',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '70769ce91a36894db98a76c0ebd9e4d86ed05c4e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phidc24v7k022/dtdm_lab4' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '70769ce91a36894db98a76c0ebd9e4d86ed05c4e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'platformsh/config-reader' => array(
+            'pretty_version' => '3.0.1',
+            'version' => '3.0.1.0',
+            'reference' => 'cab2b6c0cdfc318df09a64136957dc1fc308f418',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../platformsh/config-reader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
