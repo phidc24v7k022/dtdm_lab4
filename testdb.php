a0c402408d5ca413765935f069fe0ecc91d5e61c
hoangphi <philh@onland.tech>
2026-06-13 09:00:40 +0700

    del people

--- testdb.php
+++ testdb.php
@@ -13,39 +13,39 @@ $dsn = sprintf('mysql:host=%s;port=%d;dbname=%s', $credentials['host'], $credent
 echo "<br>".$dsn;
 
 try {
-    $conn = new \PDO($dsn, $credentials['username'], $credentials['password'], [
-        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-        \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => TRUE,
-        \PDO::MYSQL_ATTR_FOUND_ROWS => TRUE,
-    ]);
+    // $conn = new \PDO($dsn, $credentials['username'], $credentials['password'], [
+    //     \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+    //     \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => TRUE,
+    //     \PDO::MYSQL_ATTR_FOUND_ROWS => TRUE,
+    // ]);
 
-    // Tạo bảng
-    $sql = "CREATE TABLE IF NOT EXISTS People (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(30) NOT NULL,
-        city VARCHAR(30) NOT NULL
-    )";
-    $conn->query($sql);
+    // // Tạo bảng
+    // $sql = "CREATE TABLE IF NOT EXISTS People (
+    //     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    //     name VARCHAR(30) NOT NULL,
+    //     city VARCHAR(30) NOT NULL
+    // )";
+    // $conn->query($sql);
 
-    // Thêm dữ liệu
-    $sql = "INSERT INTO People (name, city) VALUES
-        ('Neil Armstrong', 'Moon'),
-        ('Buzz Aldrin', 'Glen Ridge'),
-        ('Sally Ride', 'La Jolla')";
-    $conn->query($sql);
+    // // Thêm dữ liệu
+    // $sql = "INSERT INTO People (name, city) VALUES
+    //     ('Neil Armstrong', 'Moon'),
+    //     ('Buzz Aldrin', 'Glen Ridge'),
+    //     ('Sally Ride', 'La Jolla')";
+    // $conn->query($sql);
 
-    // Truy vấn
-    $sql = "SELECT * FROM People";
-    $result = $conn->query($sql);
-    $result->setFetchMode(\PDO::FETCH_OBJ);
+    // // Truy vấn
+    // $sql = "SELECT * FROM People";
+    // $result = $conn->query($sql);
+    // $result->setFetchMode(\PDO::FETCH_OBJ);
 
-    if ($result) {
-        print "<table><thead><tr><th>Name</th><th>City</th></tr></thead><tbody>";
-        foreach ($result as $record) {
-            printf("<tr><td>%s</td><td>%s</td></tr>\n", $record->name, $record->city);
-        }
-        print "</tbody></table>";
-    }
+    // if ($result) {
+    //     print "<table><thead><tr><th>Name</th><th>City</th></tr></thead><tbody>";
+    //     foreach ($result as $record) {
+    //         printf("<tr><td>%s</td><td>%s</td></tr>\n", $record->name, $record->city);
+    //     }
+    //     print "</tbody></table>";
+    // }
 
     // Xóa bảng
     $sql = "DROP TABLE People";
